57faf33922e16efeddaef6aa8b994a4245a048bb
Srishti Agarwal <agarwal11srishti@gmail.com>
2023-10-20 15:55:16 +0530

    Update file structure

---
+++ script.js
@@ -0,0 +1,44 @@
+const accessKey = "Ol4mo_JuR_J83wBXmKJGm9Nl5KvIhk6HCxpZoBVe2lM";
+
+const searchForm = document.getElementById("search-form");
+const searchBox = document.getElementById("search-box");
+const searchResult = document.getElementById("search-result");
+const showMore = document.getElementById("show-more");
+
+let keyword = "";
+let page = 1;
+async function searchImages(){
+    keyword = searchBox.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    if(page === 1){
+        searchResult.innerHTML = "";
+    }
+
+    const results = data.results;
+    results.map((result) => {
+        const image = document.createElement("img");
+        image.src = result.urls.small;
+        const imageLink = document.createElement("a");
+        imageLink.href = result.links.html;
+        imageLink.target = "_blank";
+        imageLink.appendChild(image);
+        searchResult.appendChild(imageLink);
+    })
+
+    showMore.style.display = "block";
+}
+
+searchForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    page = 1;
+    searchImages();
+});
+
+showMore.addEventListener("click", () => {
+    page++;
+    searchImages();
+})
